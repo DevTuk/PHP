8a27b9608726b772de38137a7ece0e17babfb480
Diego <dienqn84@gmail.com>
2021-11-12 11:42:35 -0300

    Actividad Obligatoria PHP

---
+++ Actividad01/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+  
+
+<div class="recuadro">
+<h2> Actividad Práctica Obligatoria </h2>
+
+    <?php
+
+
+            $nombre = "Diego";
+            $apellido = "Barros";
+            $edad = 36;
+            $hobbie = "Video Juegos y tocar el Bajo";
+            $editor = "Visual Estudio Code";
+            $sistemaOperativo = "Windows 11";
+
+            echo "Nombre y Apellido: $nombre $apellido <br>";
+            echo "Edad:   $edad <br>";
+            echo "Hobbie:   $hobbie <br>";
+            echo "Editor Preferido:   $editor <br>";
+            echo "Sistema Operativo:   $sistemaOperativo <br>";
+            echo "Nombre:  $nombre <br>";
+
+
+    ?>
+</div>
+
+
+</body>
+</html>
+
+
+
